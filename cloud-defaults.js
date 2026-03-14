9499a7417938d65730755ead2b5035d2f2e59dcf
Alexys78 <alexis.aubard@icloud.com>
2026-03-14 17:51:02 +0100

    Add files via upload

--- cloud-defaults.js
+++ cloud-defaults.js
@@ -3,5 +3,5 @@ window.TV_MANAGER_CLOUD_DEFAULTS = window.TV_MANAGER_CLOUD_DEFAULTS || {
   anonKey: "sb_publishable_uZdsqTc6d1D-zSWTrTTD8w_iNwoVvjL",
   table: "tv_manager_state_records",
   syncToken: "default",
-  adminEmails: ["alexis.aubard@icloud.com"]
+  adminEmails: ["alexis.aubard@icloud.com", "alexis.aubard@icloud.fr"]
 };
